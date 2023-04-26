04ab71e4f04e3b08d065491840c95759fa1b1c49
Alvaro Alonso <alvaroaq@outlook.es>
2023-04-26 15:14:57 +0200

    Cambio hola mundo por estoy esperando

--- prueba.java
+++ prueba.java
@@ -1,9 +1,11 @@
 public class TestClass {
-private final String message;
-public TestClass() {
-this.message = “Hola mundo”;
-}
-public String getMessage() {
-return message;
-}
+	private final String message;
+
+	public TestClass() {
+	this.message = “Estoy esperando”;
+	}
+
+	public String getMessage() {
+		return message;
+	}
 }
